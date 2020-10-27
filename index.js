8de018ffe2199fa07729c215a2754dfdc13ff0a7
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-10-27 20:00:34 +0100

    make indexer class

--- index.js
+++ index.js
@@ -1,76 +1,96 @@
 const Nanoeth = require('nanoeth/http')
 const Tail = require('@hyperdivision/eth-transaction-tail')
 const Hyperbee = require('hyperbee')
-const hypercore = require('hypercore')
+const streamx = require('streamx')
 
-const feed = hypercore('./db')
-const db = new Hyperbee(feed, {
-  valueEncoding: 'json',
-  keyEncoding: 'utf-8'
-})
-
-const to = '0x50c7d91e74B0E42BD8bcE8AD6d199E4a23c0b193'
-
-const eth = new Nanoeth('https://ropsten.infura.io/v3/2aa3f1f44c224eff83b07cef6a5b48b5')
-
-let since = 8958600
-
-const t = new Tail(null, {
-  eth,
-  since,
-  async filter (addr) {
-    const node = await db.get('!addrs!' + addr)
-    return node !== null
-  },
-  async transaction (tx) {
-    if (!(await db.get('!addrs!' + tx.to))) return
-    console.log('transaction', tx)
-    return db.put(txKey(tx), tx)
-  },
-  checkpoint (seq) {
-    since = seq
-    console.log('checkpoint', seq)
+module.exports = class EthIndexer {
+  constructor (endpoint, feed, since) {
+    this.eth = new Nanoeth(endpoint)
+    
+    this.feed = feed
+    this.db = new Hyperbee(this.feed, {
+      valueEncoding: 'json',
+      keyEncoding: 'utf-8'
+    })
   }
-})
 
-head().then(console.log)
-track(to)
-t.start()
-catchup(100).then(() => { console.log('caught up')})
+  createTransactionStream (addr) {
+    const self = this
 
-async function head () {
-  for await (const { value } of db.createHistoryStream({ reverse: true, limit: 1 })) {
-    return value.blockNumber
+    const tr = new streamx.Transform({
+      open: () => {
+        str.push(self.db.get('!addrs!' + addr))
+      }
+    })
+
+    const range = {
+      gt: '!tx!' + addr + '!',
+      lte: '!tx!' + addr + '"'
+    }
+
+    return this.db.createReadStream(rang).pipe(tr)
   }
-  return eth.blockNumber()
-}
 
-async function catchup (minBehind) {
-  let tip = Number(await eth.blockNumber())
-  while (tip - since > minBehind) {
-    await sleep(1000)
-    tip = Number(await eth.blockNumber()) 
+  async add (addr) {
+     await this._catchup(90)
+     await this._track(addr)
   }
-}
 
-async function track (addr) {
-  const id = addr.toLowerCase()
+  async start (defaultSeq) {
+    const head = await this._head() // see my impl, should return the latest blockNumber
+    const since = Math.max(head, defaultSeq)
 
-  t.wait(async function () {
-    if (await db.get('!addrs!' + id)) return
+    const tail = new Tail({
+      eth: this.eth,
+      since,
+      async filter (addr) {
+        const node = await this.db.get('!addrs!' + addr)
+        return node !== null
+      },
+      async transaction (tx) {
+        if (!(await this.db.get('!addrs!' + tx.to))) return
+        return this.db.put(txKey(tx), tx)
+      },
+      checkpoint (seq) {
+        since = seq
+      }
+    })
+  }
 
-    const hei = Number(await eth.blockNumber())
-    if (hei - since > 100) throw new Error('Tailer is too far behind') // infura only keeps 125 blocks of history on state
+  async _head () {
+    for await (const { value } of db.createHistoryStream({ reverse: true, limit: 1 })) {
+      return value.blockNumber
+    }
+    return eth.blockNumber()
+  }
 
-    const from = '0x' + Math.max(0, since - 1).toString(16)
-    const balance = await eth.getBalance(addr, from)
+  async _catchup (minBehind) {
+    let tip = Number(await eth.blockNumber())
+    while (tip - since > minBehind) {
+      await sleep(1000)
+      tip = Number(await eth.blockNumber()) 
+    } 
+  }
 
-    await db.put('!addrs!' + id, { date: Date.now(), blockNumber: from, initialBalance: balance })
-  })
+  async _track (addr) {
+    const id = addr.toLowerCase()
+
+    t.wait(async function () {
+      if (await db.get('!addrs!' + id)) return
+
+      const hei = Number(await eth.blockNumber())
+      if (hei - since > 100) throw new Error('Tailer is too far behind') // infura only keeps 125 blocks of history on state
+
+      const from = '0x' + Math.max(0, since - 1).toString(16)
+      const balance = await eth.getBalance(addr, from)
+
+      await db.put('!addrs!' + id, { date: Date.now(), blockNumber: from, initialBalance: balance })
+    })
+  }
 }
 
 function txKey (tx) {
-  return '!tx!' + padBlockNumber(tx.blockNumber) + '!' + padTxNumber(tx.transactionIndex)
+  return '!tx!' + tx.to + '!' + padBlockNumber(tx.blockNumber) + '!' + padTxNumber(tx.transactionIndex)
 }
 
 function padTxNumber (n) {
