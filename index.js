ff838229e1a9f774fe2576090707b36c08ce795d
Christophe Diederichs <45171645+chm-diederichs@users.noreply.github.com>
2020-10-29 12:34:27 +0100

    Move EthIndexer to class and add in live TxStream (#3)
    * add catchup function
    
    * move test calls to top level
    
    * change to while loop, parseNumber before comparison
    
    * make indexer class
    
    * live stream transactions
    
    * move logic into open handler to ensure correct ordering
    
    * add TxStream class
    
    * remoe test output
    
    * remove log; return from async start; gc txns on close
    
    * await db entry in tail

--- index.js
+++ index.js
@@ -1,76 +1,177 @@
 const Nanoeth = require('nanoeth/http')
 const Tail = require('@hyperdivision/eth-transaction-tail')
 const Hyperbee = require('hyperbee')
-const hypercore = require('hypercore')
+const { Readable } = require('streamx')
+const UpsertMap = require('upsert-map')
 
-const feed = hypercore('./db')
-const db = new Hyperbee(feed, {
-  valueEncoding: 'json',
-  keyEncoding: 'utf-8'
-})
+const promiseCallback = (p, cb) => p.then(data => cb(null, data), cb)
 
-const to = '0x50c7d91e74B0E42BD8bcE8AD6d199E4a23c0b193'
+module.exports = class EthIndexer {
+  constructor (endpoint, feed) {
+    this.since = null
+    this.eth = new Nanoeth(endpoint)
+    this.tail = null
+    
+    this.feed = feed
+    this.db = new Hyperbee(this.feed, {
+      valueEncoding: 'json',
+      keyEncoding: 'utf-8'
+    })
 
-const eth = new Nanoeth('https://ropsten.infura.io/v3/2aa3f1f44c224eff83b07cef6a5b48b5')
-
-let since = 8958600
-
-const t = new Tail(null, {
-  eth,
-  since,
-  async filter (addr) {
-    const node = await db.get('!addrs!' + addr)
-    return node !== null
-  },
-  async transaction (tx) {
-    if (!(await db.get('!addrs!' + tx.to))) return
-    console.log('transaction', tx)
-    return db.put(txKey(tx), tx)
-  },
-  checkpoint (seq) {
-    since = seq
-    console.log('checkpoint', seq)
+    this.streams = new UpsertMap(() => new Set(), set => !set.size)
   }
-})
 
-head().then(console.log)
-track(to)
-t.start()
-catchup(100).then(() => { console.log('caught up')})
+  createTransactionStream (addr) {
+    const self = this
 
-async function head () {
-  for await (const { value } of db.createHistoryStream({ reverse: true, limit: 1 })) {
-    return value.blockNumber
+    const address = addr.toLowerCase()
+
+    const txns = new TxStream(this.db, address, { live: true })
+    this.streams.upsert(addr).add(txns)
+
+    txns.on('close', () => this.streams.get(addr).delete(txns))
+    return txns
   }
-  return eth.blockNumber()
+
+  async add (addr) {
+    await this._catchup(90)
+    await this._track(addr)
+  }
+
+  async start (defaultSeq) {
+    const self = this
+
+    const head = await this._head()
+    this.since = Math.max(head, defaultSeq)
+
+    this.tail = new Tail(null, {
+      eth: self.eth,
+      since: self.since,
+      async filter (addr) {
+        let address = addr ? addr.toLowerCase() : ''
+        const node = await self.db.get('!addrs!' + address)
+        return node !== null
+      },
+      async transaction (tx) {
+        const addr = tx.to.toLowerCase()
+
+        if (!(await self.db.get('!addrs!' + addr))) return
+        await self.db.put(txKey(tx), tx)
+
+        if (self.streams.has(addr)) {
+          for (let str of self.streams.get(addr)) {
+            str.pushLive(tx)
+          }
+        }
+      },
+      async checkpoint (seq) {
+        self.since = seq
+      }
+    })
+
+    return this.tail.start()
+  }
+
+  async _head () {
+    for await (const { value } of this.db.createHistoryStream({ reverse: true, limit: 1 })) {
+      return value.blockNumber
+    }
+    return this.eth.blockNumber()
+  }
+
+  async _catchup (minBehind) {
+    if (this.since === null) throw new Error('Tailer has not started')
+
+    let tip = Number(await this.eth.blockNumber())
+    while (tip - this.since > minBehind) {
+      await sleep(1000)
+      tip = Number(await this.eth.blockNumber()) 
+    }
+  }
+
+  async _track (addr) {
+    const self = this
+    const id = addr.toLowerCase()
+
+    this.tail.wait(async function () {
+      if (await self.db.get('!addrs!' + id)) return
+
+      const hei = Number(await self.eth.blockNumber())
+      if (hei - self.since > 100) throw new Error('Tailer is too far behind') // infura only keeps 125 blocks of history on state
+
+      const from = '0x' + Math.max(0, self.since - 1).toString(16)
+      const balance = await self.eth.getBalance(addr, from)
+
+      await self.db.put('!addrs!' + id, { date: Date.now(), blockNumber: from, initialBalance: balance })
+    })
+  }
 }
 
-async function catchup (minBehind) {
-  let tip = Number(await eth.blockNumber())
-  while (tip - since > minBehind) {
-    await sleep(1000)
-    tip = Number(await eth.blockNumber()) 
+class TxStream extends Readable {
+  constructor (db, addr, opts) {
+    super()
+
+    this.live = opts.live
+    this.addr = addr
+    this.pending = []
+    this.db = db
+    this.live = !!opts.live
   }
-}
+  
+  pushLive (data) {
+    if (this.pending) this.pending.push(data)
+    else this.push(data)
+  }
+  
+  _read (cb) {
+    if (this.stream) this.stream.resume()
+    cb(null)
+  }
+  
+  _open (cb) {
+    const self = this
+    promiseCallback(this.db.get('!addrs!' + this.addr), (err, val) => {
+      if (err) return cb(err)
 
-async function track (addr) {
-  const id = addr.toLowerCase()
+      self.push({
+        blockNumber: val.value.blockNumber,
+        value: val.value.initialBalance
+      })
 
-  t.wait(async function () {
-    if (await db.get('!addrs!' + id)) return
+      const gt = padKey('!tx!' + self.addrs + '!')
+      const lt = padKey('!tx!' + self.addrs + '"')
 
-    const hei = Number(await eth.blockNumber())
-    if (hei - since > 100) throw new Error('Tailer is too far behind') // infura only keeps 125 blocks of history on state
+      self.stream = self.db.createReadStream({ gt, lt })
+      
+      let lastKey = ''
+      self.stream.on('data', (data) => {
+        lastKey = data.key
+        if (!self.push(data)) self.stream.pause()
+      })
+      
+      self.stream.on('end', () => {
+        self.stream = null
 
-    const from = '0x' + Math.max(0, since - 1).toString(16)
-    const balance = await eth.getBalance(addr, from)
+        if (!self.live) {
+          self.push(null) // end it now
+          return
+        }
 
-    await db.put('!addrs!' + id, { date: Date.now(), blockNumber: from, initialBalance: balance })
-  })
+        while (self.pending.length) {
+          const next = self.pending.shift()
+          if (lastKey < txKey(next)) continue // we already emitted this
+          self.push(next)
+        }
+        self.pending = null
+      })
+
+      cb(null)
+    })
+  }
 }
 
 function txKey (tx) {
-  return '!tx!' + padBlockNumber(tx.blockNumber) + '!' + padTxNumber(tx.transactionIndex)
+  return '!tx!' + tx.to.toLowerCase() + '!' + padBlockNumber(tx.blockNumber) + '!' + padTxNumber(tx.transactionIndex)
 }
 
 function padTxNumber (n) {
@@ -79,6 +180,10 @@ function padTxNumber (n) {
 
 function padBlockNumber (n) {
   return n.slice(2).padStart(12, '0')
+}
+
+function padKey (key) {
+  return key.padEnd(68, '\u0000')
 }
 
 function sleep (n) {
---
+++ test.js
@@ -0,0 +1,17 @@
+const Indexer = require('./')
+const hypercore = require('hypercore')
+
+const feed = hypercore('./db')
+
+const to = '0x50c7d91e74b0e42bd8bce8ad6d199e4a23c0b193'
+const url = 'https://ropsten.infura.io/v3/2aa3f1f44c224eff83b07cef6a5b48b5'
+const since = 0x88ceb0 - 200
+
+const index = new Indexer(url, feed, to)
+
+index.start(since).then(() => {
+  index.add(to)
+
+  const str = index.createTransactionStream(to)
+  str.on('data', d => console.log('-- data --', d))
+})
