7bd86ee148507ffd43025ea27aa4fb06431cbb59
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-12-02 15:33:22 +0100

    update test

--- test.js
+++ test.js
@@ -7,13 +7,10 @@ const to = '0x50c7d91e74b0e42bd8bce8ad6d199e4a23c0b193'
 const url = 'https://ropsten.infura.io/v3/2aa3f1f44c224eff83b07cef6a5b48b5'
 const since = 9037562 - 200
 
-const index = new Indexer(url, feed, since)
+const index = new Indexer(feed, { endpoint: url })
 
 index.start()
 index.add(to).then(() => {
-  console.log(index.tail.tracking)
   const str = index.createTransactionStream(to)
-  str.on('data', d => {
-    console.log('-- data --', d)
-  })
+  str.on('data', d => { console.log('-- data --', d) })
 })
